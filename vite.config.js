c5df507adf9c721ce35cdad57de081cfc5067428
Liliana <lilpaimb@gmail.com>
2025-07-20 23:25:53 -0500

    vite

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
+    vite-tsconfig-paths(),
     tailwindcss(),
     tsconfigPaths(),
   ],
