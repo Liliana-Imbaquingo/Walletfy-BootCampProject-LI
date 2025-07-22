20f4b194ac97e70494f26b46f63ea74e1bf60b10
Liliana <lilpaimb@gmail.com>
2025-07-21 20:44:48 -0500

    withVariables

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-
-
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
